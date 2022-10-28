b4e4bc3d0f4deefa90a6b1098a75605ae7a1b1f4
Jesus <jdgonzalez815@misena.edu.co>
2022-10-27 19:06:41 -0500

    commit 2

--- personal/insert.php
+++ personal/insert.php
@@ -2,19 +2,20 @@
 include("conexion.php");
 $con=conectar();
 
-$id=$_POST['id'];
 $username=$_POST['username'];
 $email=$_POST['email'];
 $password=$_POST['password'];
 $role=$_POST['role'];
 
 
-$sql="INSERT INTO `mainlogin`(`id`, `username`, `email`, `password`, `role`) VALUES('$id','$username','$email','$password','$role')";
+$sql="INSERT INTO `mainlogin` (`username`, `email`, `password`, `role`) VALUES ('$username','$email','$password','$role')";
 $query= mysqli_query($con,$sql);
 
 if($query){
-    Header("Location: personal_portada.php");
+    Header("Location: admin_portada.php");
     
-}else {
+}
+else {
+
 }
 ?>
